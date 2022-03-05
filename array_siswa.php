46228f88f3d32c810daf2a6918d384318ce80e5e
Rafly Al Hakim <91861490+callmeraph@users.noreply.github.com>
2022-03-05 13:06:57 +0700

    Add files via upload

---
+++ array_siswa.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+    
+    
+
+<?php
+$ns1 = ['id'=>1,'nim'=>'01101','uts'=>80,'uas'=>84,'tugas'=>78];
+$ns2 = ['id'=>2,'nim'=>'01121','uts'=>70,'uas'=>50,'tugas'=>68];
+$ns3 = ['id'=>3,'nim'=>'01130','uts'=>60,'uas'=>86,'tugas'=>70];
+$ns4 = ['id'=>4,'nim'=>'01134','uts'=>90,'uas'=>91,'tugas'=>82];
+$ar_nilai = [$ns1, $ns2 , $ns3, $ns4];
+?>
+<h3>Daftar Nilai Siswa</h3>
+<table class="table table-dark table-striped" border="1" width="100%">
+<thead>
+<tr>
+<th>No</th><th>NIM</th><th>UTS</th>
+<th>UAS</th><th>Tugas</th><th>Nilai Akhir</th>
+</tr>
+</thead>
+<tbody>
+<?php
+$nomor = 1;
+foreach($ar_nilai as $ns){
+echo '<tr><td>'.$nomor.'</td>';
+echo '<td>'.$ns['nim'].'</td>';
+echo '<td>'.$ns['uts'].'</td>';
+echo '<td>'.$ns['uas'].'</td>';
+echo '<td>'.$ns['tugas'].'</td>';
+$nilai_akhir = ($ns['uts'] + $ns['uas']+$ns['tugas'])/3;
+echo '<td>'.number_format($nilai_akhir,2,',','.').'</td>';
+echo '<tr/>';
+$nomor++;
+}
+?>
+</tbody>
+</table>
+</body>
+
+</html>
